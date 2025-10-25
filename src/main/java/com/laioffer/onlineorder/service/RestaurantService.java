cececdd4c97789a3675538b35dbb8fa5a0889ecd
luc1ferxx <jixuxiao1@gmail.com>
2025-10-25 11:40:23 -0700

    Relocate project to C:\\MyProjects and fix build environment

--- src/main/java/com/laioffer/onlineorder/service/RestaurantService.java
+++ src/main/java/com/laioffer/onlineorder/service/RestaurantService.java
@@ -7,6 +7,7 @@ import com.laioffer.onlineorder.model.MenuItemDto;
 import com.laioffer.onlineorder.model.RestaurantDto;
 import com.laioffer.onlineorder.repository.MenuItemRepository;
 import com.laioffer.onlineorder.repository.RestaurantRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 
@@ -32,6 +33,7 @@ public class RestaurantService {
     }
 
 
+    @Cacheable("restaurants")
     public List<RestaurantDto> getRestaurants() {
         List<RestaurantEntity> restaurantEntities = restaurantRepository.findAll();
         List<MenuItemEntity> menuItemEntities = menuItemRepository.findAll();
